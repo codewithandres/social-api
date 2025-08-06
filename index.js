9dc6bbf21ba9bf97f9b2b82b929c2e975a354c70
codewithandres <axiozweb2022@gmail.com>
2025-08-05 22:18:26 -0500

    fix(cors): configure CORS to allow requests from localhost

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express();
 // ? Midelwares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
 app.use(cookieParser());
 // app.use('/api/user', (req, res) => {
 // 	res.send('Hello World!');
